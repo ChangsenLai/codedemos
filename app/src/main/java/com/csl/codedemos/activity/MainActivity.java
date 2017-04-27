2f7d58fde35999329b00e1e808894f36c7dff98d
laichangsen <laichangsen@yupaopao.cn>
2017-04-27 15:24:00 +0800

    add databinding

--- app/src/main/java/com/csl/codedemos/activity/MainActivity.java
+++ app/src/main/java/com/csl/codedemos/activity/MainActivity.java
@@ -1,16 +1,12 @@
 package com.csl.codedemos.activity;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
+import com.csl.codedemos.R;
+import com.csl.codedemos.databinding.ActivityMainBinding;
 
 /**
  * Created by ChangsenLai on 2017/4/21.
@@ -21,7 +17,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        testDrawableTextView();
+        ActivityMainBinding dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        dataBinding.setEvent(new Event());
+    }
+
+    public class Event {
+        public void testDrawable() {
+            testDrawableTextView();
+        }
     }
 
     private void init() {
